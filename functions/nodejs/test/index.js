f401dee98afe9c26ed8b9aee59f324b791601912
zhengjunqi.bytedance.com <zhengjunqi@bytedance.com>
2024-10-23 11:35:04 +0000

    1 个文件产生了变更，影响了1 个 函数
    更新函数「test」

--- functions/nodejs/test/index.js
+++ functions/nodejs/test/index.js
@@ -12,6 +12,6 @@
 module.exports = async function (params, context, logger) {
   // 日志功能
   // logger.info(`${new Date()} 函数开始执行`);
-   return{"res":"done"}
+   return{"res":"done1"}
   // 在这里补充业务代码
 }
